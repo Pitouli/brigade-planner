f83c55dbd6b082f5c61d0d4d11d2bbbd53c20ce2
Pitouli <pitouli@gmail.com>
2026-08-21 20:31:11 +0200

    Update example

--- src/data/exampleCsv.ts
+++ src/data/exampleCsv.ts
@@ -1,20 +1,42 @@
-export const EXAMPLE_CSV = `Nom;Horaire;Chefferie;Ven. dîner;Sam. déj.;Sam. dîner;Dim. déj.;Dim. dîner;Lun. déj.;Lun. dîner;Mar. déj.;Mar. dîner;Mer. déj.;Mer. dîner;Jeu. déj.;Jeu. dîner;Ven. déj.;Ven. dîner;Sam. déj.;Sam. dîner
-JB;Dîner;Jamais;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam
-Vincent;Indifférent;Toujours;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam
-Marine;Indifférent;;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam
-Jules;Déjeuner;Une fois;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam
-Antoine L;Indifférent;Toujours;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam
-Charles;Dîner;;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam
-lisa;Indifférent;Jamais;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;;;;;;;;;
-Hortense;Déjeuner;Toujours;Miam;Miam;Miam;Miam;;;;;;;;;;;;;
-Charrue;;Jamais;Miam;Miam;Miam;Miam;;;;;;;;;;;Miam;Miam;Miam
-Côme;;Jamais;;;;;;;;;;;;;Miam;Miam;Miam;Miam;Miam
-Julie L;Dîner;Jamais;;;;;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam
-Paul M;Déjeuner;Toujours;;;;;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam
-Marina 💕;Indifférent;;;;Miam;Miam;;;;;;;;;;;;;
-Philippine;Indifférent;;;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;Miam;;;
-Cécile;Dîner;;;;;;;;;Miam;Miam;Miam;Miam;Miam;;;;;
-Mathilde;;;;;Miam;Miam;Miam;Miam;Miam;Miam;Miam;;;;;;;;
-Jean;;Jamais;Miam;Miam;Miam;Miam;;;;;;;;;;;;;
-Anne-Sophie;Indifférent;Jamais;Miam;Miam;Miam;Miam;;;;;;;;;;;;;
-Antoine J;Dîner;Jamais;Miam;Miam;Miam;Miam;;;;;;;;;;;;;`;
+export const EXAMPLE_CSV = `Nom		Départ de	le	Retour à	le		Horaire	Chefferie	Exempté			Jeu. dîner	Ven. déj.	Ven. dîner	Sam. déj.	Sam. dîner	Dim. déj.	Dim. dîner	Lun. déj.	Lun. dîner	Mar. déj.	Mar. dîner	Mer. déj.	Mer. dîner	Jeu. déj.	Jeu. dîner	Ven. déj.	Ven. dîner	Sam. déj.	Sam. dîner	Dim. déj.	Dim. dîner	Lun. déj.
+Armand		Viroflay	Jeudi	Viroflay	Dimanche 2		Dîner	Jamais	FALSE					Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam		
+Bernard		Viroflay	Jeudi	Viroflay	Dimanche 2		Indifférent	Toujours	FALSE					Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam		
+Chloé		Paris	Vendredi	Paris	Dimanche 2		Déjeuner	1 fois	FALSE					Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam			
+Diego		Paris	Vendredi	Paris	Dimanche 2		Indifférent	1 fois	FALSE					Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam		
+Emilie		Paris	Vendredi	Paris	Dimanche 2		Dîner	Jamais	FALSE					Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam		
+François		Paris	Vendredi	Paris	Dimanche 2		Déjeuner	Toujours	FALSE					Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam		
+Gaëlle		Paris	Vendredi	Paris	Mardi		Indifférent	Jamais	FALSE					Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam											
+Hortense		Paris	Vendredi	Paris	Lundi		Déjeuner	Jamais	FALSE					Miam	Miam	Miam	Miam	Miam	Miam														
+Inès		Montpellier	Vendredi	Montpellier	Dimanche 2		Indifférent	Jamais	FALSE					Miam	Miam	Miam	Miam											Miam	Miam	Miam	Miam		
+Joël		Paris	Jeudi 2	Paris	Dimanche 2			Jamais	FALSE																	Miam	Miam	Miam	Miam	Miam	Miam		
+Karim		Rochefort	Dimanche	Rochefort	Dimanche 2		Indifférent	1 fois	FALSE									Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam			
+Magalie		Rochefort	Dimanche	Rochefort	Dimanche 2		Indifférent	Toujours	FALSE									Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam			
+Noémie		Metz	Samedi	Metz	Vendredi 2				TRUE								Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam							
+Ophélie		Metz	Samedi	Metz	Vendredi 2				TRUE								Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam							
+Patrick		Metz	Samedi	Metz	Vendredi 2				TRUE																								
+Quentin		Metz	Samedi	Metz	Vendredi 2				TRUE																								
+Raoul		Maisons-Laffitte	Samedi	Maisons-Laffitte	Dimanche				FALSE							Miam	Miam																
+Simon		Paris	Samedi	Paris	Jeudi				FALSE						Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam						
+Thomas		Paris	Mardi	Paris	Jeudi 2				FALSE												Miam	Miam	Miam	Miam	Miam								
+Ursule		Clermont	Samedi	Clermont	Mercredi		Déjeuner	1 fois	FALSE							Miam	Miam	Miam	Miam	Miam	Miam	Miam											
+Victor		Paris	Vendredi	Paris	Dimanche				FALSE					Miam	Miam	Miam	Miam																
+William		Paris	Vendredi	Paris	Dimanche		Indifférent	1 fois	FALSE					Miam	Miam	Miam	Miam																
+Xavier		Paris	Vendredi	Paris	Dimanche				FALSE					Miam	Miam	Miam	Miam																
+Yvonne		Marseille	Dimanche	Marseille	Vendredi 2				TRUE									Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam	Miam						
+Zoé		Lyon	Dimanche	Lyon	Vendredi 2				TRUE																								
+Jean		Nantes	Dimanche	Nantes	Vendredi 2				TRUE																								
+Baptiste		Rennes	Dimanche	Rennes	Vendredi 2				TRUE																								
+																																	
+Nb personnes				Nb Dodos							Total	0	0	12	13	15	17	15	15	14	15	15	13	13	13	13	11	10	10	10	7	0	0
+27		Nuit de	Jeudi	2																													
+		Nuit de	Vendredi	12							Tâcherons requis	0	0	4,8	5,2	6	6,8	6	6	5,6	6	6	5,2	5,2	5,2	5,2	4,4	4	4	4	2,8	0	0
+		Nuit de	Samedi	18							Chef de brigade			Bernard		Ursule							Noémie										
+		Nuit de	Dimanche	20												Victor																	
+		Nuit de	Lundi	19																													
+		Nuit de	Mardi	19																													
+		Nuit de	Mercredi	18																													
+		Nuit de	Jeudi 2	18																													
+		Nuit de	Vendredi 2	10																													
+		Nuit de	Samedi 2	10																													
+		Nuit de	Dimanche 2	0																													
+		Nuit de	Lundi 2	0																													`;
